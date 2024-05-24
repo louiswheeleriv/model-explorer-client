97120bf4d813a60b61a58eefb4aa720e00c56716
Louis Wheeler <louiswheeleriv@gmail.com>
2024-05-24 07:15:54 -0400

    Try node deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import node from "@sveltejs/adapter-node";
 
 const config = {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+		adapter: node({ env: { port: process.env.PORT } })
+	}
 };
 
 export default config;
